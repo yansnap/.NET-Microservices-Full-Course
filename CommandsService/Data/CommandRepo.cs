3f5b88ef32c2c8c76b82a8789d2e036a54ee5150
Yan <jan.kozhyn@gmail.com>
2023-04-22 21:05:31 +0300

    Data Layer - Repository

---
+++ CommandsService/Data/CommandRepo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public class CommandRepo : ICommandRepo
+    {
+        private readonly AppDbContext _context;
+
+        public CommandRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _context.Platforms.ToList();
+        }
+
+        public void CreatePlatform(Platform plat)
+        {
+            if (plat == null)
+            {
+                throw new ArgumentNullException(nameof(plat));
+            }
+            _context.Platforms.Add(plat);
+        }
+
+        public bool PlatformExists(int platformId)
+        {
+            return _context.Platforms.Any(p => p.Id == platformId);
+        }
+
+        public IEnumerable<Command> GetCommandsForPlatform(int platformId)
+        {
+            return _context.Commands
+                .Where(c => c.PlatformId == platformId)
+                .OrderBy(c => c.Platform.Name);
+        }
+
+        public Command GetCommand(int platformId, int commandId)
+        {
+            return _context.Commands.FirstOrDefault(c => c.PlatformId == platformId && c.Id == commandId);
+        }
+
+        public void CreateCommand(int platformId, Command command)
+        {
+            if (command == null)
+            {
+                throw new ArgumentNullException(nameof(command));
+            }
+            command.PlatformId = platformId;
+            _context.Commands.Add(command);
+        }
+    }
+}
