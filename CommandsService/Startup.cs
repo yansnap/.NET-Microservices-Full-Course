254ccf83c5a7328bdab6ae0eecce33b3a748292e
Yan <jan.kozhyn@gmail.com>
2023-04-24 01:55:58 +0300

    GG WP

--- CommandsService/Startup.cs
+++ CommandsService/Startup.cs
@@ -57,6 +57,8 @@ namespace CommandsService
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            
+            PrepDb.PrepPopulation(app);
         }
     }
 }
